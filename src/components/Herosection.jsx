e372299b611a0e9f045bf2707d4caa3c98e7e8c4
syed asim <aasimshah8110@gmail.com>
2023-06-17 00:05:40 +0500

    234d

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -9,10 +9,10 @@ function Herosection() {
         
         <div className="w-6/12  h-44">
             <p className="text-lg font-black">
-                Hey&apos; I'm Aasim shah
+                Hey&apos; I am Aasim shah
             </p>
             <p className=" font-semibold mt-4">Welcome to my digital Garden ☘🥀</p>
-            <p className=" font-semibold  mt-3">I am a software developer from Pakistan&apos; I make open-source projects webpages and native apps  and write about code&apos; design&apos; and life.</p>
+            <p className=" font-semibold  mt-3">I am a software developer from Pakistan&apos; I make open-source projects webpages and native apps  and write about code&apos; design&apos; and life</p>
         </div>
         {theme && theme==="light" ?
         <Image  src={'/assets/1.jpg'} className='ml-32' width={350} height={350}/>
